8e620ac955bb25d9716764cf7c4b71cf053cee83
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-27 12:57:43 +0900

    fizzbuzzの実装をする

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(number) {
+  if (number % 15 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
+}
+
+for (let i = 1; i <= 20; i++) {
+  console.log(fizzbuzz(i));
+}
