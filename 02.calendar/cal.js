0735f54dd654b6d587f33bb78f0b289823480eab
YuSa0-6 <fuku.sugar@icloud.com>
2024-06-12 15:53:05 +0900

    左辺が０の場合にも右辺が評価されてしまうので？？演算子を使用する。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,8 +2,8 @@ import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
-const year = argv.y || today.getFullYear();
-const month = argv.m || today.getMonth() + 1;
+const year = argv.y ?? today.getFullYear();
+const month = argv.m ?? today.getMonth() + 1;
 
 console.log(`      ${month}月${year}`);
 console.log("日 月 火 水 木 金 土");
