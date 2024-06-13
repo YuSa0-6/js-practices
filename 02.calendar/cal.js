639661be8e9810989c6b38c10d77dd3b36b3823f
YuSa0-6 <fuku.sugar@icloud.com>
2024-06-13 11:48:55 +0900

    年月表記を標準コマンドに合わせるために、半角空白を入れる。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,7 +7,7 @@ const today = new Date();
 const year = argv.y ?? today.getFullYear();
 const month = argv.m ?? today.getMonth() + 1;
 
-console.log(`      ${month}月${year}`);
+console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 const firstDay = new Date(`${month} ${1} ${year}`).getDate();
 const lastDay = new Date(year, month, 0).getDate();
